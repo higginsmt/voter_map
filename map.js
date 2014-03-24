1da45421e036ce6c68cd781c28ff53f18251f9c4
Matthew Higgins <higginsmt@gmail.com>
2014-03-24 15:18:43 -0400

    initial commit with choropleth map

---
+++ map.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+  var width = 960,
+      height = 500;
+
+  var rateById = d3.map();
+
+  var quantize = d3.scale.quantize()
+      .domain([0,1])
+      // pre-built range from 0-8, mapping it to the class-name from CSS
+      .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+
+  var path = d3.geo.path();
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  queue()
+      .defer(d3.json, "us.json")
+      .defer(d3.csv, "votes.csv", function(d) { rateById.set(d.id, +d.rate); })
+      .await(ready);
+
+  function ready(error, us) {
+    svg.append("g")
+        .attr("class", "counties")
+      .selectAll("path")
+        .data(topojson.feature(us, us.objects.counties).features)
+      .enter().append("path")
+        .attr("class", function(d) {
+          return quantize(rateById.get(d.id)); })
+        .attr("d", path);
+
+    svg.append("path")
+        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .attr("class", "states")
+        .attr("d", path);
+  }
+};
